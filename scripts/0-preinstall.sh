9bd9acecfc57ffe1e57cd7cfdeb0c1c2e34369b0
Deinorius <94495331+Deinorius@users.noreply.github.com>
2022-07-30 17:18:20 +0200

    Update 0-preinstall.sh

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -55,11 +55,11 @@ echo -ne "
 umount -A --recursive /mnt # make sure everything is unmounted before we start
 # disk prep
 sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+sgdisk -o ${DISK} # new gpt disk 2048 alignment | sgdisk -a 2048
 
 # create partitions
-sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-sgdisk -n 2::+550M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
+sgdisk -n 1::+1M --typecode=1:EF02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
+sgdisk -n 2::+550M --typecode=2:EF00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
 sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
 if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
     sgdisk -A 1:set:2 ${DISK}
@@ -180,7 +180,7 @@ if [[ ! -d "/sys/firmware/efi" ]]; then
     grub-install --boot-directory=/mnt/boot ISK}
 else
     pacstrap /mnt efibootmgr --noconfirm --needed
-#    sed -i '52 s/.*/HOOKS="base systemd keyboard autodetect sd-vconsole modconf block filesystems fsck"/' /mnt/etc/mkinitcpio.conf
+    sed -i '52 s/.*/HOOKS="base systemd keyboard autodetect sd-vconsole modconf block filesystems fsck"/' /mnt/etc/mkinitcpio.conf
 fi
 echo -ne "
 -------------------------------------------------------------------------
@@ -199,7 +199,6 @@ TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
     # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
     echo "# /swap/swapfile" >> /mnt/etc/fstab
     echo "/swap/swapfile   none	swap	defaults	0	0" >> /mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
-fi
 echo -ne "
 -------------------------------------------------------------------------
                     SYSTEM READY FOR 1-setup.sh
