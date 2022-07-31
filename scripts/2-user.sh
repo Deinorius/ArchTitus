1877e7197098ad09d3df3203d6a82a16a30010af
Deinorius <94495331+Deinorius@users.noreply.github.com>
2022-07-31 14:48:08 +0200

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -39,7 +39,7 @@ do
 done
 
 
-if [[ ! $AUR_HELPER == none ]]; then
+if [[ ! $AUR_HELPER == pamac-nosnap ]]; then
   cd ~
   git clone "https://aur.archlinux.org/$AUR_HELPER.git"
   cd ~/$AUR_HELPER
@@ -54,6 +54,27 @@ if [[ ! $AUR_HELPER == none ]]; then
     fi
     echo "INSTALLING: ${line}"
     $AUR_HELPER -S --noconfirm --needed ${line}
+  done
+fi
+
+if [[ ! $AUR_HELPER == none || ! $AUR_HELPER == pamac-nosnap]]; then
+  cd ~
+  git clone "https://aur.archlinux.org/$AUR_HELPER.git"
+  cd ~/$AUR_HELPER
+  makepkg -si --noconfirm
+  # sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
+  # stop the script and move on, not installing any more packages below that line
+  sed -n '/'$INSTALL_TYPE'/q;p' ~/ArchTitus/pkg-files/aur-pkgs.txt | while read line
+  do
+    if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
+      # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+      continue
+    fi
+    echo "INSTALLING: ${line}"
+    if [[ $AUR_HELPER == pamac-nosnap ]]; then
+      $AUR_HELPER install --no-confirm ${line}pamac --needed
+    else
+      $AUR_HELPER -S --noconfirm --needed ${line}
   done
 fi
 
