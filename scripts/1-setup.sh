8f9222fca2897027fb7d88ae4ebc4f095219eec6
Deinorius <94495331+Deinorius@users.noreply.github.com>
2022-08-03 17:45:46 +0200

    Update 1-setup.sh

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -57,7 +57,7 @@ sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 timedatectl --no-ask-password set-timezone ${TIMEZONE}
 timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+#localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
 ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 hwclock --systohc
 systemctl enable systemd-timesyncd.service
