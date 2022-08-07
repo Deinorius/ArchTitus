c09fbcc9c207c1452f20afc1a997c5afa0ccb51b
Deinorius <94495331+Deinorius@users.noreply.github.com>
2022-08-07 17:31:03 +0200

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -35,7 +35,8 @@ if [[ ${SHELL} == "zsh-manjaro" ]]; then
    git clone https://github.com/Deinorius/deino-zshconf && cd deino-zshconf && makepkg -si --noconfirm && cd ~
    rm -rf deino-zshconf nerd-fonts-noto-sans-mono
    cp /etc/skel/.zshrc /home/$USERNAME/
-   chsh -s /bin/zsh $USERNAME
+   sed -i 's/\/bin\/bash/\/bin\/zsh/' /etc/passwd
+   #chsh -s /bin/zsh $USERNAME
    
 elif [[ ${SHELL} == "zsh-Titusprofile" ]]; then
    cd ~
