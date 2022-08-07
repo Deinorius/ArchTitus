dd21fd72b377c91770902c3279c46ce2ddc3df26
Deinorius <94495331+Deinorius@users.noreply.github.com>
2022-08-07 15:52:20 +0200

    Update archtitus.sh

--- archtitus.sh
+++ archtitus.sh
@@ -26,11 +26,11 @@ echo -ne "
     ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
       source $CONFIGS_DIR/setup.conf
     ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
+    ( systemd-spawn -b -D /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
     if [[ ! $DESKTOP_ENV == server ]]; then
-      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
+      ( systemd-spawn -b -D /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
     fi
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    ( systemd-spawn -b -D /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
     cp -v *.log /mnt/home/$USERNAME
 
 echo -ne "
