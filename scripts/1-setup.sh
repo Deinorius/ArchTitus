838580b3f1681f52c36da89a27063bf881e716ab
Deinorius <94495331+Deinorius@users.noreply.github.com>
2022-09-02 15:23:42 +0200

    Update 1-setup.sh

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -60,8 +60,10 @@ hwclock --systohc
 systemctl enable systemd-timesyncd.service
 
 # Set keymaps
-sed -i 's/^#de_AT.UTF-8 UTF-8/de_AT.UTF-8 UTF-8/' /etc/locale.gen
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^# *\(de_AT.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
+#sed -i 's/^#de_AT.UTF-8 UTF-8/de_AT.UTF-8 UTF-8/' /etc/locale.gen
+#sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 
 echo KEYMAP=${KEYMAP} > /etc/vconsole.conf
